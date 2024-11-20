36998f0c14b61a868ad42bff822fb8980ded144d
Krasstalker <160205105+Krasstalker@users.noreply.github.com>
2024-11-20 16:37:14 +0700

    Update example.py

--- example.py
+++ example.py
@@ -6,15 +6,15 @@ il = Z2("127.0.0.1",9999)
 # TIME FORMAT HH:MM:SS
 # card_version = 0
 # room_number = 101
-il_station : int = 1
+station_num : int = 1
 
 # USAGE EXAMPLE
 READ_CARD : list = ["<STX>","<SEP>",station_num,"<SEP>","RC","<SEP>","<ETX>","<LRC>"] #READ CARD
 # CHECKOUT: list = ["<STX>","<SEP>",station_num,"<SEP>","CO","<SEP>",room_num,"<SEP>",0,"<SEP>",0,"<SEP>","<ETX>","<LRC>"] #CHECKOUT
 # CHECKIN : list = [f"<STX>","<SEP>",station_num,"<SEP>","CI","<SEP>",room_num,"<SEP>",date_from,"<SEP>",time_from,"<SEP>",date_to,"<SEP>",time_to,"<SEP>","<SEP>","<SEP>","RP","<SEP>","Reception","<SEP>","<SEP>","<SEP>","""common_doors::0,description::custom door,place::0,typeid::4,guest_card_version::{card_version},service::0,room::{room_number},startdate::{date_from},starttime::{time_from},enddate::{date_to},endtime::{time_to},EmMarine::keep""","<SEP>","<ETX>","<LRC>"] #CHECKIN
-# CHECKIN_EX : list = ["<STX>","<SEP>",il_station,"<SEP>","CX","<SEP>",room_number,"<SEP>", date_from ,"<SEP>",time_from, "<SEP>", date_to, "<SEP>",time_to,"<SEP>","<ETX>","<LRC>"] #CHECKIN_EX
-# COPYCARD : list = [f"<STX>","<SEP>",il_station,"<SEP>","CG","<SEP>",room_number,"<SEP>",date_from,"<SEP>",time_from,"<SEP>",date_to,"<SEP>",time_to,"<SEP>","<SEP>","<SEP>","RP","<SEP>","Reception","<SEP>","<SEP>","<SEP>","""EmMarine::keep,starttime::{time_from},room::{room_number},startdate::{date_from},service::0,enddate::{date_to},description::custom door,typeid::4,place::0,guest_card_version::{card_version},endtime::{time_to}""","<SEP>","<ETX>"] #COPYCARD
-# EMERGENCYCARD : list = ["<STX>","<SEP>",il_station,"<SEP>","EM","<SEP>",room_number,"<SEP>",start_date,"<SEP>",start_time,"<SEP>",end_date,"<SEP>",end_time,"<SEP>","<ETX>","<LRC>"] #EMERGENCYCARD
+# CHECKIN_EX : list = ["<STX>","<SEP>",station_num,"<SEP>","CX","<SEP>",room_number,"<SEP>", date_from ,"<SEP>",time_from, "<SEP>", date_to, "<SEP>",time_to,"<SEP>","<ETX>","<LRC>"] #CHECKIN_EX
+# COPYCARD : list = [f"<STX>","<SEP>",station_num,"<SEP>","CG","<SEP>",room_number,"<SEP>",date_from,"<SEP>",time_from,"<SEP>",date_to,"<SEP>",time_to,"<SEP>","<SEP>","<SEP>","RP","<SEP>","Reception","<SEP>","<SEP>","<SEP>","""EmMarine::keep,starttime::{time_from},room::{room_number},startdate::{date_from},service::0,enddate::{date_to},description::custom door,typeid::4,place::0,guest_card_version::{card_version},endtime::{time_to}""","<SEP>","<ETX>"] #COPYCARD
+# EMERGENCYCARD : list = ["<STX>","<SEP>",station_num,"<SEP>","EM","<SEP>",room_number,"<SEP>",start_date,"<SEP>",start_time,"<SEP>",end_date,"<SEP>",end_time,"<SEP>","<ETX>","<LRC>"] #EMERGENCYCARD
 # USAGE EXAMPLE
 
 
